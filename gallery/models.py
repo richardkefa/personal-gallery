b000cfc1d1145062b3385d5622886029d62a75c6
richardkefa <richardkefa@gmail.com>
2020-10-09 23:55:28 +0300

    add image location and category model clases

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+  image = models.CharField()
+  image_name = models.CharField()
+  Image_description = models.TextField()
+  lodation = models.ForeignKey(Location)
+  category = models.ForeignKey(Category)
+  
+  def __str__(self):
+    return self.image
+  
+  def save_image(self):
+    self.save()
+  
+  
+class Location(models.Model):
+  location = models.CharField(max_length=50)
+  
+class Category(models.Model):
+  category = models.CharField()
+  
