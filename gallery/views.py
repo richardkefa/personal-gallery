8652327a8673eb8b113581c363a572c9ba1e641c
richardkefa <richardkefa@gmail.com>
2020-10-13 15:49:24 +0300

    add search_resuts view function

--- gallery/views.py
+++ gallery/views.py
@@ -14,3 +14,14 @@ def image(request,image_id):
     raise Http404()
   return render(request, 'full-gallery/pic.html',{"image":image})
 
+def search_results(request):
+  if 'category' in request.GET and request.GET["category"]:
+    search_term = request.GET.get("category")
+    searched_categories = Category.search_by_catetegory(search_term)
+    message = f"{search_term}"
+    
+    return render(request,'full-gallery/search.html',{"message":message,"categories":searched_categories})
+  
+  else:
+    message = "You did not search for any term"
+    return render(request,"full-gallery/search.html",{"message":message})
