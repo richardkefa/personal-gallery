43903c4177bf8dc169a7463fe8f728e8f7bbcb79
richardkefa <richardkefa@gmail.com>
2020-10-14 10:12:33 +0300

    deploy to heroku

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,10 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static"),
-]
 
+#Extra places for collectstatic to find sataic files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#configure file location for media
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#Configure django App for Heroku
+django_heroku.settings(locals())
